7447ff7b47d2c682db222100856f560a63d90630
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2018-06-07 19:06:43 -0300

    changed structure

---
+++ include/max30100.h
@@ -0,0 +1,401 @@
+/**
+ * @file max30100.h
+ * 
+ * @author
+ * Angelo Elias Dalzotto (150633@upf.br)
+ * GEPID - Grupo de Pesquisa em Cultura Digital (http://gepid.upf.br/)
+ * Universidade de Passo Fundo (http://www.upf.br/)
+ * 
+ * @copyright
+ * Copyright (c) 2017 Raivis Strogonovs (https://morf.lv)
+ * 
+ * @brief This library was created to interface the MAX30100 pulse and oxymeter
+ * sensor with ESP32 using the IDF-SDK. It includes functions to initialize with
+ * the programmer's desired parameters and to update the readings, detecting pulse
+ * and having the pulse saturation O2. It is based on Strogonovs Arduino library.
+ */
+
+#ifndef MAX30100_H
+#define MAX30100_H
+
+#include <cmath>
+#include <memory>
+#include <driver/i2c.h>
+#include <esp_log.h>
+#include "i2c_device.h"
+#include "FreeRTOS.h"
+
+namespace Max30100 {
+    class Device;
+
+    enum class Mode;
+    enum class SamplingRate;
+    enum class PulseWidth;
+    enum class Current;
+    enum class PulseState;
+}
+
+/**
+ * Operation Mode Enum.
+ * Heart rate only or Oxigen saturation + Heart rate.
+ */
+enum class Max30100::Mode {
+    HR_ONLY = 0x02,
+    SPO2_HR = 0x03
+};
+
+/**
+ * Sampling rate enum.
+ * Internal sampling rates from 50Hz up to 1KHz.
+ */
+enum class Max30100::SamplingRate {
+    RATE_50HZ   = 0x00,
+    RATE_100HZ  = 0x01,
+    RATE_167HZ  = 0x02,
+    RATE_200HZ  = 0x03,
+    RATE_400HZ  = 0x04,
+    RATE_600HZ  = 0x05,
+    RATE_800HZ  = 0x06,
+    RATE_1000HZ = 0x07
+};
+
+/**
+ * Led pulse width enum.
+ * Sets from 200us to 1600us.
+ */
+enum class Max30100::PulseWidth {
+    WIDTH_200US_ADC_13  = 0x00,
+    WIDTH_400US_ADC_14  = 0x01,
+    WIDTH_800US_ADC_15  = 0x02,
+    WIDTH_1600US_ADC_16 = 0x03
+};
+
+/**
+ * Led current enum.
+ * Sets the current for any of the leds. From 0mA to 50mA.
+ */
+enum class Max30100::Current {
+    CURRENT_0MA    = 0x00,
+    CURRENT_0MA    = 0x00,
+    CURRENT_4_4MA  = 0x01,
+    CURRENT_7_6MA  = 0x02,
+    CURRENT_11MA   = 0x03,
+    CURRENT_14_2MA = 0x04,
+    CURRENT_17_4MA = 0x05,
+    CURRENT_20_8MA = 0x06,
+    CURRENT_24MA   = 0x07,
+    CURRENT_27_1MA = 0x08,
+    CURRENT_30_6MA = 0x09,
+    CURRENT_33_8MA = 0x0A,
+    CURRENT_37MA   = 0x0B,
+    CURRENT_40_2MA = 0x0C,
+    CURRENT_43_6MA = 0x0D,
+    CURRENT_46_8MA = 0x0E,
+    CURRENT_50MA   = 0x0F
+};
+
+/**
+ * Pulse State Machine
+ */
+enum class Max30100::PulseState {
+    IDLE,
+    TRACE_UP,
+    TRAC_DOWN
+};
+
+/**
+ * Main device class
+ */
+class Max30100::Device {
+public:
+    /**
+     * @brief Device constructor. Must have I2C already initialized.
+     * 
+     * @details All parameters are optional.
+     * 
+     * @param i2c_num is the I2C port of the ESP32 with the MAX30100 is attached to.
+     * @param mode is the working mode of the sensor (HR only or HR+SPO2)
+     * @param sampling_rate is the frequency which samples are taken internally.
+     * @param pulse_width is the led pulse width. 
+     * @param ir_current is the current to the IR Led.
+     * @param start_red_current is the starting value to Red Led current.
+     * @param acceptable_intensity_diff TODO
+     * @param red_current_adj_ms Minimum time to update red led current (ms).
+     * @param rst_spo2_pulse_n TODO
+     * @param alpha Filter parameter.
+     * @param mean_filter_sz is the sampling vector size to the filter.
+     * @param pulse_min_thresh TODO
+     * @param pulse_max_thresh TODO
+     * @param bpm_sample_size is the heart rate sampling vector size.
+     * @param high_res_mode is to set if high resolution mode or not.
+     * @param debug is to set if registers output will be done to serial monitoring.
+     * 
+     * @throw std::bac_alloc Case can't allocate memory for sample buffers.
+     * @throw I2CExcept::CommandFailed Case some i2c communication fail.
+     */
+    Device(i2c_port_t i2c_num = I2C_NUM_0, 
+            Mode mode = Mode::SPO2_HR, 
+            SamplingRate sampling_rate = SamplingRate::RATE_100HZ,
+            PulseWidth pulse_width = PulseWidth::WIDTH_1600US_ADC_16,
+            Current ir_current = Current::CURRENT_50MA,
+            Current start_red_current = Current::CURRENT_27_1MA,
+            uint32_t acceptable_intensity_diff = 65000,
+            uint16_t red_current_adj_ms = 500, uint8_t rst_spo2_pulse_n = 4,
+            double alpha = 0.95, uint8_t mean_filter_sz = 15,
+            uint16_t pulse_min_thresh = 300, uint16_t pulse_max_thresh = 2000,
+            uint8_t bpm_sample_size = 10, bool high_res_mode = true, 
+            bool debug = false);
+
+    /**
+     * @brief Reads the MAX30100 registers and update the object.
+     * 
+     * @details This must be called at the same rate as sampling rate defined.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void update();
+
+    /**
+     * @brief Reads the internal temperature of the MAX30100.
+     * 
+     * @returns Temperature in celsius.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    double read_temperature();
+
+    /**
+     * @brief Prints the registers for debugging purposes.
+     * 
+     * @throw
+     *     - std::runtime_error Case not in debug mode
+     *     - I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void print_registers();
+
+    /**
+     * @brief Sets the operation mode. HR Only or HR+SPO2.
+     * 
+     * @param mode is the mode of operation desired.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void set_mode(Mode mode = Mode::SPO2_HR);
+
+    /**
+     * @brief Sets the resolution. High or standard.
+     * 
+     * @param high_res is if high resolution will be enabled.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void set_high_res(bool high_res = true);
+
+    /**
+     * @brief Sets the led currents.
+     * 
+     * @param red_current is the current value of the Red Led.
+     * @param ir_current is the current value of the IR Led.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void set_led_current(Current red_current = Current::CURRENT_27_1MA,
+                            Current ir_current = Current::CURRENT_50MA);
+
+    /**
+     * @brief Sets the pulse width.
+     * 
+     * @param pw is the pulse width desired.
+     */
+    void set_pulse_width(PulseWidth pw = PulseWidth::WIDTH_1600US_ADC_16);
+
+    /**
+     * @brief Set the internal sampling rate.
+     * 
+     * @param rate is the sampling rate desired.
+     * 
+     * @throw I2CExcept::CommandFailed Case some i2c comm fails.
+     */
+    void set_sampling_rate(SamplingRate rate = SamplingRate::RATE_100HZ);
+
+    double get_bpm();
+
+    double get_spo2(); 
+
+private:
+    /**
+     * FIFO reading structure.
+     */
+    struct Fifo {
+        uint16_t raw_ir;
+        uint16_t raw_red;
+    };
+
+    /**
+     * DC filter results.
+     */
+    struct Filter {
+        double w;
+        double result;
+    };
+
+    /**
+     * Butterworth filter results.
+     */
+    struct Butterworth {
+        double v[2];
+        double result;
+    };
+
+    /**
+     * Mean diff filter results.
+     */
+    struct MeanDiff {
+        std::unique_ptr<double[]> values;
+        uint8_t i;
+        double sum;
+        uint8_t count;
+    };
+
+    /**
+     * @brief Pulse detection algorithm.
+     * 
+     * @param sensor_value is the value read from the sensor after the filters
+     * 
+     * @returns true if detected.
+     */
+    bool detect_pulse(double sensor_value);
+
+    /**
+     * @brief Balance intensities filter.
+     * 
+     * @details DC filter for the raw values.
+     * 
+     * @param red_dc is the w in red led dc filter structure.
+     * @param ir_dc is the w in ir led dc filter structure.
+     */
+    void balance_intensities(double red_dc, double ir_dc);
+
+    /**
+     * @brief Removes the DC offset of the sensor value.
+     * 
+     * @param x is the raw value read from the led.
+     * @param prev_w is the previous filtered w from the dc filter structure.
+     * @param alpha is the dc filter alpha parameter.
+     * 
+     * @returns a dc filter structure.
+     */
+    struct Filter dc_removal(double x, double prev_w, double alpha);
+
+    /**
+     * @brief Applies the mean diff filter.
+     * 
+     * @param M is the filtered DC result of the dc filter structure.
+     * 
+     * @returns a filtered value.
+     */
+    double mean_diff(double M);
+
+    /**
+     * @brief Applies a low-pass butterworth filter.
+     * 
+     * @param this is the address of the configuration structure.
+     * @param x is the mean diff filtered value.
+     */
+    void lpb_filter(double x);
+
+    I2CDevice i2c;
+
+    i2c_port_t i2c_num;
+    uint32_t acceptable_intensity_diff;
+    Current red_current;
+    uint32_t red_current_adj_ms;
+    uint8_t rst_spo2_pulse_n;
+    double dc_alpha;
+    uint16_t pulse_min_threshold;
+    uint16_t pulse_max_threshold;
+
+    uint8_t mean_filter_size;
+    uint8_t pulse_bpm_sample_size;
+
+    bool debug;
+    PulseState current_pulse_state;
+
+    struct MeanDiff mean_diff_ir;
+    std::unique_ptr<double[]> values_bpm;
+
+    Current ir_current;
+
+    double last_red_current_check;    
+    double current_bpm;
+    double bpm_sum;
+    uint8_t bpm_count;
+    uint8_t bpm_i;
+
+    struct Fifo prev_fifo;
+    struct Filter dc_filter_ir;
+    struct Filter dc_filter_red;
+    struct Butterworth lpb_filter_ir;
+    
+    double ir_ac_sq_sum;
+    double red_ac_sq_sum;
+    uint16_t samples_recorded;
+    uint16_t pulses_detected;
+    double current_spO2;
+
+    bool pulse_detected;
+    double heart_bpm;
+    double ir_cardiogram;
+    double ir_dc_val;
+    double red_dc_val;
+    double spo2;
+    uint32_t last_beat_thresh;
+    double dc_filtered_red;
+    double dc_filtered_ir;
+
+    static const uint8_t DEV_ADDR;
+    static const uint8_t REV_ID;
+    static const uint8_t PART_ID;
+    static const uint8_t INT_STATUS;
+    static const uint8_t INT_ENABLE;
+    static const uint8_t FIFO_WRITE;
+    static const uint8_t FIFO_OVF_COUNTER;
+    static const uint8_t FIFO_READ;
+    static const uint8_t FIFO_DATA;
+    static const uint8_t MODE_CONF;
+    static const uint8_t SPO2_CONF;
+    static const uint8_t LED_CONF;
+    static const uint8_t TEMP_INT;
+    static const uint8_t TEMP_FRAC;
+
+    static const uint8_t MODE_SHDN;
+    static const uint8_t MODE_RST;
+    static const uint8_t MODE_TEMP_EN;
+    static const uint8_t SPO2_HI_RES_EN;
+
+    static const char *TAG;
+};
+
+#endif
+
+/**
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
---
+++ max30100.cpp
@@ -0,0 +1,397 @@
+#include "max30100.h"
+
+const uint8_t Max30100::Device::DEV_ADDR = 0x57;
+const uint8_t Max30100::Device::REV_ID = 0xFE;
+const uint8_t Max30100::Device::PART_ID = 0xFF;
+const uint8_t Max30100::Device::INT_STATUS = 0x00;
+const uint8_t Max30100::Device::INT_ENABLE = 0x01;
+const uint8_t Max30100::Device::FIFO_WRITE = 0x02;
+const uint8_t Max30100::Device::FIFO_OVF_COUNTER = 0x03;
+const uint8_t Max30100::Device::FIFO_READ = 0x04;
+const uint8_t Max30100::Device::FIFO_DATA = 0x05;
+const uint8_t Max30100::Device::MODE_CONF = 0x06;
+const uint8_t Max30100::Device::SPO2_CONF = 0x07;
+const uint8_t Max30100::Device::LED_CONF = 0x09;
+const uint8_t Max30100::Device::TEMP_INT = 0x16;
+const uint8_t Max30100::Device::TEMP_FRAC = 0x17;
+
+const uint8_t Max30100::Device::MODE_SHDN = (1 << 7);
+const uint8_t Max30100::Device::MODE_RST = (1 << 6);
+const uint8_t Max30100::Device::MODE_TEMP_EN = (1 << 3);
+const uint8_t Max30100::Device::SPO2_HI_RES_EN = (1 << 6);
+
+const char *Max30100::Device::TAG = "Max30100";
+
+Max30100::Device::Device(i2c_port_t i2c_num, Mode mode,
+                            SamplingRate sampling_rate, PulseWidth pulse_width,
+                            Current ir_current, Current start_red_current,
+                            uint32_t acceptable_intensity_diff,
+                            uint16_t red_current_adj_ms,
+                            uint8_t rst_spo2_pulse_n, double alpha,
+                            uint8_t mean_filter_sz, uint16_t pulse_min_thresh,
+                            uint16_t pulse_max_thresh, uint8_t bpm_sample_size,
+                            bool high_res_mode, bool debug) :
+                            i2c(DEV_ADDR, i2c_num),
+                            i2c_num(i2c_num),
+                            acceptable_intensity_diff(acceptable_intensity_diff),
+                            red_current_adj_ms(red_current_adj_ms),
+                            rst_spo2_pulse_n(rst_spo2_pulse_n),
+                            dc_alpha(alpha),
+                            pulse_min_threshold(pulse_min_thresh),
+                            pulse_max_threshold(pulse_max_thresh),
+                            mean_filter_size(mean_filter_sz),
+                            pulse_bpm_sample_size(bpm_sample_size),
+                            debug(debug),
+                            current_pulse_state(PulseState::IDLE),
+                            red_current(start_red_current),
+                            last_red_current_check(0),
+                            ir_current(ir_current)
+{
+    try {
+        mean_diff_ir.values.reset(new double[mean_filter_size]);
+        values_bpm.reset(new double[mean_filter_size]);
+    } catch(const std::bad_alloc& ex){
+        throw ex;
+    }
+
+    try {
+        set_mode(mode);
+        set_sampling_rate(sampling_rate);
+        set_pulse_width(pulse_width);
+        set_led_current(red_current, ir_current);
+        set_high_res(high_res_mode);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+
+    dc_filter_ir.w = 0;
+    dc_filter_ir.result = 0;
+
+    dc_filter_red.w = 0;
+    dc_filter_red.result = 0;
+
+    lpb_filter_ir.v[0] = 0;
+    lpb_filter_ir.v[1] = 0;
+    lpb_filter_ir.result = 0;
+
+    mean_diff_ir.i = 0;
+    mean_diff_ir.sum = 0;
+    mean_diff_ir.count = 0;
+
+    mean_diff_ir.i = 0;
+    mean_diff_ir.sum = 0;
+    mean_diff_ir.count = 0;
+
+    ir_ac_sq_sum = 0;
+    red_ac_sq_sum = 0;
+    samples_recorded = 0;
+    pulses_detected = 0;
+    current_spO2 = 0;
+
+    last_beat_thresh = 0;
+}
+
+void Max30100::Device::set_mode(Mode mode)
+{
+    try {
+        i2c.modify_register(MODE_CONF, 0xF8, (uint8_t)mode);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+}
+
+void Max30100::Device::set_sampling_rate(SamplingRate rate)
+{
+    try {
+        i2c.modify_register(SPO2_CONF, 0xE3, (uint8_t)rate << 2);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+}
+
+void Max30100::Device::set_pulse_width(PulseWidth pw)
+{
+    try {
+        i2c.modify_register(SPO2_CONF, 0xFC, (uint8_t)pw);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+}
+
+void Max30100::Device::set_led_current(Current red_current, Current ir_current)
+{
+    try {
+        i2c.write_register(LED_CONF, ((uint8_t)red_current << 4) | (uint8_t)ir_current);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+}
+
+void Max30100::Device::set_high_res(bool high_res)
+{
+    try {
+        if(high_res)
+            i2c.modify_register(SPO2_CONF, 0xFF, SPO2_HI_RES_EN);
+        else
+            i2c.modify_register(SPO2_CONF, ~SPO2_HI_RES_EN, 0x00);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+}
+
+void Max30100::Device::update()
+{
+    pulse_detected = false;
+    heart_bpm = 0.0;
+    ir_cardiogram = 0.0;
+    ir_dc_val = 0.0;
+    red_dc_val = 0.0;
+    last_beat_thresh = 0;
+    dc_filtered_ir = 0.0;
+    dc_filtered_red = 0.0;
+
+    Fifo raw_data;
+    
+    try {
+        uint8_t buffer[4];
+        i2c.read_buffer(FIFO_DATA, buffer, 4);
+        raw_data.raw_ir = (uint16_t)(buffer[0] << 8) | buffer[1];
+        raw_data.raw_red = (uint16_t)(buffer[2] << 8) | buffer[3];
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+
+    dc_filter_ir = dc_removal((double)raw_data.raw_ir, dc_filter_ir.w, dc_alpha);
+    dc_filter_red = dc_removal((double)raw_data.raw_red, dc_filter_red.w, dc_alpha);
+
+    lpb_filter(mean_diff(dc_filter_ir.result) /*-dc_filter_ir.result*/);
+
+    ir_ac_sq_sum += dc_filter_ir.result * dc_filter_ir.result;
+    red_ac_sq_sum += dc_filter_red.result * dc_filter_red.result;
+    samples_recorded++;
+
+    if(detect_pulse(lpb_filter_ir.result) && samples_recorded){
+        pulse_detected = true;
+        pulses_detected++;
+
+        double ratio_rms = log(sqrt(red_ac_sq_sum / (double)samples_recorded)) /
+                            log(sqrt(ir_ac_sq_sum / (double)samples_recorded));
+
+        if(debug)
+            ESP_LOGD(TAG, "RMS Ratio: %d\n", ratio_rms);
+
+        //This is my adjusted standard model, so it shows 0.89 as 94% saturation.
+        //It is probably far from correct, requires proper empirical calibration.
+        current_spO2 = 110.0 - 18.0 * ratio_rms;
+
+        if(!(pulses_detected % rst_spo2_pulse_n)){
+            ir_ac_sq_sum = 0;
+            red_ac_sq_sum = 0;
+            samples_recorded = 0;
+        }
+    }
+}
+
+struct Max30100::Device::Filter Max30100::Device::dc_removal(double x, double prev_w, double alpha)
+{
+    struct Filter filtered;
+    filtered.w = x + alpha * prev_w;
+    filtered.result = filtered.w - prev_w;
+
+    return filtered;
+}
+
+void Max30100::Device::lpb_filter(double x)
+{
+    lpb_filter_ir.v[0] = lpb_filter_ir.v[1];
+
+    //Fs = 100Hz and Fc = 10Hz
+    lpb_filter_ir.v[1] = (2.452372752527856026e-1 * x) +
+                            (0.50952544949442879485 * lpb_filter_ir.v[0] );
+
+    //Fs = 100Hz and Fc = 4Hz
+    //lpb_filter_ir.v[1] = (1.367287359973195227e-1 * x)
+    //                   + (0.72654252800536101020 * lpb_filter_ir.v[0]);
+    //Very precise butterworth filter
+
+    lpb_filter_ir.result = lpb_filter_ir.v[0] + lpb_filter_ir.v[1];
+}
+
+double Max30100::Device::mean_diff(double M)
+{
+    double avg = 0;
+
+    mean_diff_ir.sum -= mean_diff_ir.values[mean_diff_ir.i];
+    mean_diff_ir.values[mean_diff_ir.i] = M;
+    mean_diff_ir.sum += mean_diff_ir.values[mean_diff_ir.i];
+
+    mean_diff_ir.i++;
+    mean_diff_ir.i = mean_diff_ir.i % mean_filter_size;
+
+    if(mean_diff_ir.count < mean_filter_size)
+        mean_diff_ir.count++;
+
+    avg = mean_diff_ir.sum / mean_diff_ir.count;
+    return avg - M;
+}
+
+
+bool Max30100::Device::detect_pulse(double sensor_value)
+{
+    static float prev_sensor_value = 0;
+    static uint8_t values_went_down = 0;
+    static uint32_t current_beat = 0;
+    static uint32_t last_beat = 0;
+
+    if(sensor_value > pulse_max_threshold) {
+        current_pulse_state = PulseState::IDLE;
+        prev_sensor_value = 0;
+        last_beat = 0;
+        current_beat = 0;
+        values_went_down = 0;
+        last_beat_thresh = 0;
+        return false;
+    }
+
+    switch(current_pulse_state) {
+    case PulseState::IDLE:
+        if(sensor_value >= this->pulse_min_threshold) {
+            current_pulse_state = PulseState::TRACE_UP;
+            values_went_down = 0;
+        }
+        break;
+    case PulseState::TRACE_UP:
+        if(sensor_value > prev_sensor_value) {
+            current_beat = (uint32_t)(xTaskGetTickCount() * portTICK_PERIOD_MS);
+            last_beat_thresh = sensor_value;
+        } else {
+            if(debug) {
+                printf("Peak reached: %f %f\n",
+                       sensor_value,
+                       prev_sensor_value);
+            }
+
+            uint32_t beat_duration = current_beat - last_beat;
+            last_beat = current_beat;
+
+            float raw_bpm = 0;
+            if(beat_duration > 0)
+                raw_bpm = 60000.0 / (float)beat_duration;
+
+            if(this->debug)
+                printf("%f\n", raw_bpm);
+
+            //This method sometimes glitches,
+            //it's better to go through whole moving average everytime.
+            //IT's a neat idea to optimize the amount of work for moving avg,
+            //but while placing, removing finger it can screw up
+            //this->values_bpmSum -= this->values_bpm[bpmIndex];
+            //this->values_bpm[bpmIndex] = rawBPM;
+            //this->values_bpmSum += this->values_bpm[bpmIndex];
+
+            values_bpm[bpm_i] = raw_bpm;
+            bpm_sum = 0;
+            for(int i = 0; i < pulse_bpm_sample_size; i++)
+                bpm_sum += values_bpm[i];
+
+            if(debug) {
+                printf("CurrentMoving Avg: ");
+
+                for(int i = 0; i < pulse_bpm_sample_size; i++)
+                    printf("%f ", values_bpm[i]);
+
+                printf(" \n");
+            }
+
+            bpm_i++;
+            bpm_i = bpm_i % pulse_bpm_sample_size;
+
+            if(bpm_count < pulse_bpm_sample_size)
+                bpm_count++;
+
+            current_bpm = bpm_sum / bpm_count;
+
+            if(debug)
+                printf("AVg. BPM: %f\n", current_bpm);
+
+            current_pulse_state = PulseState::TRAC_DOWN;
+
+            return true;
+        }
+        break;
+    case PulseState::TRAC_DOWN:
+        if(sensor_value < prev_sensor_value)
+            values_went_down++;
+
+        if(sensor_value < pulse_min_threshold)
+            current_pulse_state = PulseState::IDLE;
+
+        break;
+    }
+
+    prev_sensor_value = sensor_value;
+    return false;
+}
+
+double Max30100::Device::read_temperature()
+{
+    try {
+        i2c.modify_register(MODE_CONF, 0xFF, MODE_TEMP_EN);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+
+    //@TODO
+    //This can be changed to a while loop, (with interrupt flag!)
+    //there is an interrupt flag for when temperature has been read.
+    FreeRTOS::sleep(100); // Change this!!
+
+    int8_t temp;
+    double temp_frac;
+    try {
+        temp = i2c.read_register(TEMP_INT);
+        temp_frac = i2c.read_register(TEMP_FRAC);
+    } catch(const I2CExcept::CommandFailed& ex){
+        throw ex;
+    }
+
+    temp_frac *= 0.0625;
+
+    return (double)temp+temp_frac;
+}
+
+void Max30100::Device::print_registers()
+{
+    if(!debug)
+        throw std::runtime_error("Not in debug mode");
+
+    try {
+        ESP_LOGD(TAG, "INT_STATUS: %x\n", i2c.read_register(INT_STATUS));
+        ESP_LOGD(TAG, "INT_EN: %x\n", i2c.read_register(INT_ENABLE));
+        ESP_LOGD(TAG, "FIFO_WRITE: %x\n", i2c.read_register(FIFO_WRITE));
+        ESP_LOGD(TAG, "FIFO_OVF_CNT: %x\n", i2c.read_register(FIFO_OVF_COUNTER));
+        ESP_LOGD(TAG, "FIFO_READ: %x\n", i2c.read_register(FIFO_READ));
+        ESP_LOGD(TAG, "FIFO_DATA: %x\n", i2c.read_register(FIFO_DATA));
+        ESP_LOGD(TAG, "MODE_CONF: %x\n", i2c.read_register(MODE_CONF));
+        ESP_LOGD(TAG, "SPO2_CONF: %x\n", i2c.read_register(SPO2_CONF));
+        ESP_LOGD(TAG, "LED_CONF: %x\n", i2c.read_register(LED_CONF));
+        ESP_LOGD(TAG, "TEMP_INT: %x\n", i2c.read_register(TEMP_INT));
+        ESP_LOGD(TAG, "TEMP_FRAC: %x\n", i2c.read_register(TEMP_FRAC));
+        ESP_LOGD(TAG, "REV_ID: %x\n", i2c.read_register(REV_ID));
+        ESP_LOGD(TAG, "PART_ID: %x\n", i2c.read_register(PART_ID));
+    } catch(const I2CExcept::CommandFailed &ex){
+        throw ex;
+    }
+}
+
+//@TODO mutex
+double Max30100::Device::get_bpm()
+{
+    return current_bpm;
+}
+
+double Max30100::Device::get_spo2()
+{
+    return current_spO2;
+}
+
