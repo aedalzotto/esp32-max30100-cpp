6f7b3b08047d209564e1a069eb796ea2433fdf43
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2018-06-08 08:50:28 -0300

    Updated third-party license

--- include/max30100.h
+++ include/max30100.h
@@ -385,6 +385,8 @@ private:
 /**
  * MIT License
  * 
+ * Copyright (c) 2017 Raivis Strogonovs (https://morf.lv)
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
